08d455f6ec7e6286a124cf4480f3f1c1637ac4e3
Jesse <jessechen002@gmail.com>
2023-02-09 20:40:16 -0500

    Project 1

--- Book.cpp
+++ Book.cpp
@@ -3,96 +3,49 @@
 // 2/6 changes
 
 Book::Book() {
-    Title = "";
-    Author = "";
-    PageCount = 0;
-    Digital = false; 
+    title_ = "";
+    author_ = "";
+    pageCount_ = 0;
+    digital_ = false; 
 }
 
-Book::Book(const string &Title, const string &Author, const int &PageCount, const bool &Digital) {
-    this->Title = Title; 
-    this->Author = Author; 
-    this->PageCount = PageCount; 
-    this->Digital = Digital; 
+Book::Book(const string& Title, const string& Author, const int& PageCount, const bool& Digital) {
+    this->title_ = Title; 
+    this->author_ = Author; 
+    this->pageCount_ = PageCount; 
+    this->digital_ = Digital; 
 }
 
 string Book::getTitle() const {
-    return Title; 
+    return title_; 
 }
 
 string Book::getAuthor() const {
-    return Author; 
+    return author_; 
 }
 
 int Book::getPageCount() const {
-    return PageCount; 
+    return pageCount_; 
 }
 
 bool Book::isDigital() const { 
-    return Digital; 
+    return digital_; 
 }
 
-void Book::setTitle(const string &newTitle) {
-    Title = newTitle; 
+void Book::setTitle(const string& new_Title_) {
+    title_ = new_Title_; 
 }
 
-void Book::setAuthor(const string &newAuthor) {
-    Author = newAuthor; 
+void Book::setAuthor(const string& new_Author_) {
+    author_ = new_Author_; 
 }
 
-void Book::setPageCount(const int &newPageCount) {
-    if (newPageCount > 0) {
-        PageCount = newPageCount;
+void Book::setPageCount(const int& new_Page_Count_) {
+    if (new_Page_Count_ > 0) {
+        pageCount_ = new_Page_Count_;
     }
 }
 
-void Book::setDigital(const bool &newDigital) {
-    Digital = newDigital; 
-}
-
-int main() {
-    std::cout << "TEST 1: TEST DEFAULT CONSTRUCTOR & ACCESSORS" << std::endl;
-
-    // Test default constructor
-    Book my_book;
-
-    // Test accessor functions
-    std::cout << my_book.getTitle() << std::endl;
-    std::cout << my_book.getAuthor() << std::endl;
-    std::cout << my_book.getPageCount() << std::endl;
-    std::cout << my_book.isDigital() << std::endl;
-
-
-    std::cout << "\nTEST 2: TEST PARAMETERIZED CONSTRUCTOR & ACCESSORS" << std::endl;
-
-    // Test parameterized constructor
-    Book my_book1("Harry Potter and the Chamber of Secrets", "J.K Rowling", 253, 1);
-    Book my_book2("Moby Dick", "Herman Melville", 378);
-
-    // Test accessor functions
-    std::cout << my_book1.getTitle() << " By " << my_book1.getAuthor()
-        << " with page count " << my_book1.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book1.isDigital() << std::endl;
-
-    std::cout << my_book2.getTitle() << " By " << my_book2.getAuthor()
-        << " with page count " << my_book2.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book2.isDigital() << std::endl;
-
-
-    std::cout << "\nTEST 3: TEST MUTATOR FUNCTIONS" << std::endl;
-
-    // Test mutator functions
-    Book my_book3("Moby Dick", "Herman Melville", 378);
-    my_book3.setTitle("50 shades of gray");
-    my_book3.setAuthor("E. L. James");
-    my_book3.setPageCount(514);
-    my_book3.setPageCount(-10);
-    my_book3.setDigital();
-
-    // Test accessor functions
-    std::cout << my_book3.getTitle() << " By " << my_book3.getAuthor()
-        << " with page count " << my_book3.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book3.isDigital() << std::endl;
-
-    return 0; 
+void Book::setDigital() { // when call this function, Digital becomes true
+    digital_ = true; 
 }
--- Book.hpp
+++ Book.hpp
@@ -5,31 +5,26 @@
 #include <string>
 using namespace std; 
 
-// Every Book has a title, an author, the page count, 
-// and finally a boolean to indicate whether or not that book is available digitally.
-
 class Book
 {
-    private: 
-        string Title;
-        string Author;
-        int PageCount;
-        bool Digital;
-
     public: //only accessor and mutator functions
-
         Book();
-        Book(const string &Title, const string &Author, const int &PageCount, const bool &Digital = false);
+        Book(const string& Title, const string& Author, const int& PageCount, const bool& Digital = false);
     
         string getTitle() const;
         string getAuthor() const;
         int getPageCount() const;
         bool isDigital() const;
 
-        void setTitle(const string &Title);
-        void setAuthor(const string &Author);
-        void setPageCount(const int &PageCount);
-        void setDigital(const bool &Digital = false);
+        void setTitle(const string& new_Title_);
+        void setAuthor(const string& new_Author_);
+        void setPageCount(const int& new_Page_Count_);
+        void setDigital();
+        
+    private: 
+        string title_;
+        string author_;
+        int pageCount_;
+        bool digital_;
 };
-
 #endif
